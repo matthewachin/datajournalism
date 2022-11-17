04c91004d40d1e923e2e2105fed866a454407dc8
Ben Levy <77113355+blevy23@users.noreply.github.com>
2022-11-17 14:20:32 -0500

    Moved files and changed character to state

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let character_info = JSON.parse(fs.readFileSync('../data/data.json', 'utf8'));
+let state_info = JSON.parse(fs.readFileSync('../data/data.json', 'utf8'));
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
-let character_template = fs.readFileSync('views/character.ejs', 'utf8');
+let state_template = fs.readFileSync('views/state.ejs', 'utf8');
 
 /*
-  1) Generate a web page for each character
+  1) Generate a web page for each state
   2) Keep track of the link for index.html
 */
-for (character in character_info){
-  let character_html = ejs.render(character_template, {
-    filename: __dirname + '/views/character.ejs',
-    stats: character_info[character],
-    name: character
+for (state in state_info){
+  let state_html = ejs.render(state_template, {
+    filename: __dirname + '/views/state.ejs',
+    stats: state_info[state],
+    name: state
   });
-  character_info[character].link = getBetterFileName(character);
-  fs.writeFileSync('../public/'+character_info[character].link+'.html', character_html, 'utf8');
+  state_info[state].link = getBetterFileName(state);
+  fs.writeFileSync('../public/'+state_info[state].link+'.html', state_html, 'utf8');
 
 }
 
 /*
-  1) Generate an index page of all characters
+  1) Generate an index page of all states
 */
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
-  data: character_info
+  data: state_info
 });
 
 fs.writeFileSync('../public/index.html', index_html, 'utf8');
 
-function getBetterFileName(characterName){
-  let betterFileName = characterName.split(" ").join("_");
+function getBetterFileName(stateName){
+  let betterFileName = stateName.split(" ").join("_");
   betterFileName = betterFileName.split(".").join("");
   betterFileName = betterFileName.split("(").join("");
   betterFileName = betterFileName.split(")").join("");
