389d5fb83b64396dc91c167655c5f51d20ac19ec
matthewachin <matthew.chin24@trinityschoolnyc.org>
2022-11-30 09:51:55 -0500

    Update grabData.js

--- data/grabData.js
+++ data/grabData.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 
 let oldFile = fs.readFileSync('test.csv', 'utf8')
-let data = fs.readFileSync('data.json', 'utf8')
+let data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+
 let states = oldFile.split('\r\n')
 let stateNames = {}
 for(state of states){
   stateNames[String(state.split(',')[1])] = {}
 }
-for(state in stateNames){
-  let stateData = data[state]['2020']
-  let population = Number(stateData.population)
-  let total_deaths = 0
-  for(cause in stateData){
-    if(cause !== 'population'){
-      state
-    }
-  }
+let fString = ''
+for(state in data){
+  console.log(state)
+  let stateData = data[state]['2020 ']
+  console.log(stateData)
+  let population = Number(stateData['population'])
+  let total_deaths = Number(stateData['total_deaths'])
+  console.log('Population: ' + String(population) + ', Deaths: ' + String(total_deaths))
 }
