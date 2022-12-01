2f74b615bbb435e15d884e301e8d4893accc28a8
matthewachin <matthew.chin24@trinityschoolnyc.org>
2022-11-30 23:14:24 -0500

    map

--- data/grabData.js
+++ data/grabData.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+/*const fs = require('fs')
 
 let oldFile = fs.readFileSync('test.csv', 'utf8')
 let data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
@@ -16,3 +16,63 @@ for(state in data){
 //  console.log(String(state) + 'Population: ' + String(population) + ', Deaths: ' + String(total_deaths))
   console.log()
 }
+*/
+const stateNames = {
+  AK: "Alaska",
+  AL: "Alabama",
+  AR: "Arkansas",
+  AZ: "Arizona",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DC: "District Of Columbia",
+  DE: "Delaware",
+  FL: "Florida",
+  GA: "Georgia",
+  GU: "Guam",
+  HI: "Hawaii",
+  IA: "Iowa",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  MA: "Massachusetts",
+  MD: "Maryland",
+  ME: "Maine",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MO: "Missouri",
+  MS: "Mississippi",
+  MT: "Montana",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  NE: "Nebraska",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NV: "Nevada",
+  NY: "New York",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VA: "Virginia",
+  VT: "Vermont",
+  WA: "Washington",
+  WI: "Wisconsin",
+  WV: "West Virginia",
+  WY: "Wyoming"
+}
+let n = {}
+for(t in stateNames){
+  n[String(t).toLowerCase()]=stateNames[t]
+}
+console.log(n)
