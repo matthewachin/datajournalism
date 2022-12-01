fb5f4d909aa0ad7882c1e1f2b7763f6eaca5e88e
Ben Levy <77113355+blevy23@users.noreply.github.com>
2022-12-01 14:32:18 -0500

    Oh no

--- data/generateJSON.js
+++ data/generateJSON.js
@@ -2,11 +2,30 @@ const fs = require('fs')
 // Notes,State,State Code,ICD Chapter,ICD Chapter Code,Year,Year Code,Deaths,Population,Crude Rate
 
 let final_data = {}
+let causesOfDeath = [
+"Certain infectious and parasitic diseases",
+"Neoplasms",
+"Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism",
+"Endocrine, nutritional and metabolic diseases",
+"Mental and behavioural disorders",
+"Diseases of the nervous system",
+"Diseases of the circulatory system",
+"Diseases of the respiratory system",
+"Diseases of the digestive system",
+"Diseases of the skin and subcutaneous tissue",
+"Diseases of the musculoskeletal system and connective tissue",
+"Diseases of the genitourinary system",
+"Certain conditions originating in the perinatal period",
+"Congenital malformations, deformations and chromosomal abnormalities",
+"Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified",
+"External causes of morbidity and mortality"
+]
+
 
 let old_file = fs.readFileSync('raw_data.txt', 'utf8')
 
 let deathData = old_file.split('\n')
-deathData.shift()
+
 
 deathData.forEach(function(death){
   let state_info = death.split('|')
@@ -44,9 +63,23 @@ for(state_name in final_data){
         total_deaths += Number(deaths)
       }
     }
+    for(cause of causesOfDeath){
+      if(cause !== 'population'){
+        if(final_data[state_name][year][cause] == null){
+          final_data[state_name][year][cause] = {}
+          final_data[state_name][year][cause]['deaths'] = 0
+          final_data[state_name][year][cause]['crude-rate'] = 0
+        }else if(final_data[state_name][year][cause]['deaths'] == null || final_data[state_name][year][cause]['crude-rate'] == null){
+          final_data[state_name][year][cause]['deaths'] = 0
+          final_data[state_name][year][cause]['crude-rate'] = 0
+        }
+      }
+    }
     state_2['total_deaths'] = total_deaths
   }
 }
+
+
 
 
 fs.writeFileSync('data.json', JSON.stringify(final_data), 'utf8')
